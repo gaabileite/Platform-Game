f06cef10e3ded66b16265ff38424066dbbf540a2
Gabriela Leite <gabileite2007@gmail.com>
2026-06-16 15:47:28 -0300

    Modify Shot class to accept velocity parameters

--- classes/shot.py
+++ classes/shot.py
@@ -4,11 +4,11 @@ from constants import *
 from classes.movable import *
 
 class Shot(Movable):
-    def __init__(self, x, y):
+    def __init__(self, x, y, vx, vy):
         super().__init__(x, y, shot_color, 0, shot_image, shot_speed, 0)
  
-        self.vx = shot_speed
-        self.vy = shot_speed
+        self.vx = vx
+        self.vy = vy
     
     def move_bullet(self):
         self.x += self.vx
