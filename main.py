ec3f4750af7817d8ee29f405cb689e3e6079b1f7
Gabriela Leite <gabileite2007@gmail.com>
2026-06-17 19:47:56 -0300

    Implement game over state and enemy interactions
    Added game over state and enemy collision handling.

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ surface = pygame.Surface((internal_width, internal_height))
 clock = pygame.time.Clock()
 
 camera = Camera(internal_width * scale)
+game_over = False
 
 player = Player(player_starter_x, player_starter_y)
-enemy = Enemy(300, 100 , hater_life)
+enemies = [
+    Enemy(300, 100 , hater_life)]
 platforms = [
     Platform(0, 160, 320, 20),
     Platform(100, 120, 80, 10),
@@ -34,28 +36,46 @@ while True:
             pygame.quit()
             exit()
 
-    player.handle_movement()
-    shot = player.shoot()
-    if shot:
-        shots.append(shot)
-    camera.follow(player)
+    if not game_over.state:
+        player.handle_movement()
+        shot = player.shoot()
+        if shot:
+            shots.append(shot)
+        camera.follow(player)
+    
+        player.update(surface, platforms)
+                
+        surface.fill(background_color)
+        pygame.draw.rect(surface, player.color, camera.apply(player))
+        pygame.draw.rect(surface, enemy_color, camera.apply(enemy))
+    
+        for platform in platforms:
+            pygame.draw.rect(surface, platform.color, camera.apply(platform))
+        for shot in shots:
+            pygame.draw.rect(surface, shot.color, camera.apply(shot))
+    
+        for shot in shots[:]:
+            shot.move_bullet()
+            if shot.x < 0 or shot.x > internal_width or shot.y < 0 or shot.y > internal_height:
+                shots.remove(shot)
 
-    player.update(surface, platforms)
-            
-    surface.fill(background_color)
-    pygame.draw.rect(surface, player.color, camera.apply(player))
-    pygame.draw.rect(surface, enemy_color, camera.apply(enemy))
+        enemy_dead = 0
+        for enemy in enemies:
+            if player.check_collision(enemy):
+                player.life -= 1
+            if player.life == 0:
+                game_over.state = True
+                break
 
-    for platform in platforms:
-        pygame.draw.rect(surface, platform.color, camera.apply(platform))
-    for shot in shots:
-        pygame.draw.rect(surface, shot.color, camera.apply(shot))
+            if shot:
+                if shot.check_collision(enemy):
+                    enemy.life -= 1
+                if enemy.life == 0:
+                    enemy_dead += 1
 
-    for shot in shots[:]:
-        shot.move_bullet()
-        if shot.x < 0 or shot.x > internal_width or shot.y < 0 or shot.y > internal_height:
-            shots.remove(shot)
-
+        for i in range(enemy_dead):
+            _ = enemies.pop(i)
+    
     scaled = pygame.transform.scale(surface, (window_width, window_height))
     screen.blit(scaled, (0, 0))
     clock.tick(60)
