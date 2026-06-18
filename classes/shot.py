4dcefa597556e76ccb8c22b26b83eb6badd755dd
Gabriela Leite <gabileite2007@gmail.com>
2026-06-18 16:36:30 -0300

    Refactor Shot class constructor and movement

--- classes/shot.py
+++ classes/shot.py
@@ -4,12 +4,10 @@ from constants import *
 from classes.movable import *
 
 class Shot(Movable):
-    def __init__(self, x, y, vx, vy):
+    def __init__(self, x, y, direction):
         super().__init__(x, y, shot_color, 0, shot_image, shot_speed, 0)
  
-        self.vx = vx
-        self.vy = vy
+        self.direction = direction
     
     def move_bullet(self):
-        self.x += self.vx
-        self.y += self.vy
+        self.move(self.direction)
