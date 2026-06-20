dca3fb04925cdcd3b9f47ce2ea92af11c833fb3c
Gabriela Leite <gabileite2007@gmail.com>
2026-06-20 20:16:42 -0300

    Fix shot

--- classes/shot.py
+++ classes/shot.py
@@ -1,13 +1,14 @@
-import pygame
-from pygame.locals import *
 from constants import *
 from classes.movable import *
 
 class Shot(Movable):
     def __init__(self, x, y, direction):
-        super().__init__(x, y, shot_color, 0, shot_image, shot_speed, 0)
+        super().__init__(x, y, shot_color, 0, shot_speed, 0, shot_size, shot_size)
  
         self.direction = direction
     
     def move_bullet(self):
         self.move(self.direction)
+
+    def update(self):
+        self.move_bullet()
