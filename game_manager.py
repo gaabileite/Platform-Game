57ee910e6890733b5ae3d97ee2cc9b25297e67d3
Gabriela Leite <gabileite2007@gmail.com>
2026-06-22 12:07:59 -0300

    Update and rename classes/game_manager.py to game_manager.py

---
+++ game_manager.py
@@ -0,0 +1,29 @@
+class GameManager:
+    def __init__(self):
+            self.current_phase = 0
+            self.states = {
+                0: 'Game Start',
+                1: 'Rezende',
+                2: 'Zé Felipe',
+                3: 'Vini Jr.',
+                4: 'Phase Transition',
+                5: 'Collectable Check',
+                6: 'Game Over',
+                7: 'Victory'
+            }
+
+    # Verifica se o jogador atingiu os requisitos para avançar para a próxima fase
+    def check_progress(self, player):
+        if self.current_phase == 1 and player.follower_count >= 200:
+            self.current_phase = 1.5
+            player.life += 1
+
+        elif self.current_phase == 2 and player.follower_count >= 450:
+            self.current_phase = 2.5
+            player.life += 5
+
+        elif self.current_phase == 3 and player.follower_count >= 700:
+            self.current_phase = 5
+
+        if player.life <= 0 or (player.follower_count < 0 and self.current_phase >= 1):
+            self.current_phase = 6
